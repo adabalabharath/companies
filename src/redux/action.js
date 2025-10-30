17f6255dca2aea695c350f6bc1112622762505b3
adabalabharath <saibharath.adabala@gmail.com>
2025-10-30 18:12:14 +0530

    static json

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { DATA_SUCCESS, ERROR, LOADING, SORT_ASC, SORT_DES } from "./actionTypes";
 
+
 export const fetchData =()=> async (dispatch) => {
     try {
       dispatch({ type: LOADING });
-      const response = await axios.get("http://localhost:5000/companies");
-      setTimeout(()=>dispatch({ type: DATA_SUCCESS, payload: response.data }),2000)
+      const response = await axios.get("/public/db.json");
+      setTimeout(()=>dispatch({ type: DATA_SUCCESS, payload: response.data.companies }),2000)
       
     } catch (error) {
       dispatch({ type: ERROR });
